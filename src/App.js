768fff687f4ed9bc8d59e2e71444fb9352de094e
Daniel DeFrance <daniel.defrance@montana.edu>
2022-10-31 11:59:02 -0600

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Counter from "./components/Counter";
+import MyGitHub from "./components/MyGitHub";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Hello React!
+      </h1>
+      <h2>
+        Daniel DeFrance - h48c334
+      </h2>
+
+      <hr />
+      <Counter /><hr />
+      <MyGitHub /><hr />
+
     </div>
   );
 }
