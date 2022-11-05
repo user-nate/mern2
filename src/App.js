6c4d028c6c7bd2c810e469688bfe95c4ac98c4bb
user-nate <54906125+nateMSU@users.noreply.github.com>
2022-11-05 15:46:17 -0600

    hopethisworks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import Counter from "./components/Counter";
+import MyCounter from "./components/MyCounter";
 import MyGitHub from "./components/MyGitHub";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>
-        Hello React!
-      </h1>
-      <h2>
-        Nathan Sheffels j67q156
-      </h2>
+    return (
+        <div className="App">
+            <h1>
+                Hello React!
+            </h1>
+            <h2>
+                Nathan Sheffels j67q156
+            </h2>
 
-      <hr />
-      <Counter incBy={1}/><hr />
-      <Counter incBy={2} decBy={3}/><hr />
-      <MyGitHub /><hr />
+            <hr />
+            <MyCounter incBy={1}/><hr />
+            <MyCounter incBy={2} decBy={3}/><hr />
+            <MyGitHub /><hr />
 
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
